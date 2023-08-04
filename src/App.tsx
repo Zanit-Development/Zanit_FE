97196eb8a7dd6b3772ea50adac3ab45a2b7b56a8
jypark38 <jury124@naver.com>
2023-08-04 09:49:23 +0900

    #21 feat : recoil 설치 및 RecoilRoot 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import GlobalStyle from "./style/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+
 import { Home } from "./pages/Home";
 import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/404" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
