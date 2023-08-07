d502d4be221b276ead2930662380418ddcbd8f86
ungHeung <t01047377477@gmail.com>
2023-08-07 11:39:45 +0900

    #29 feat : placeholder, button value 값 추가, 변경

---
+++ src/libs/constants/values/outputValues.ts
@@ -0,0 +1,26 @@
+export const BUTTON_VALUE = {
+  GO_HOME: "홈으로 가기",
+  SUBSCRIBES: "지금 구독하기",
+  SEARCH_BAR: "지금 이용 가능한 칵테일바 찾기",
+
+  SIGN: {
+    IN: "로그인",
+    UP: "회원가입",
+  },
+
+  COUPON: {
+    USE: "쿠폰 바로 사용하기",
+    ZAN: "ZAN 쿠폰 사용하기",
+  },
+
+  PAYMENT: {
+    CONFIRM: "입금 확인 요청하기",
+    APPROVE: "결제 승인하기",
+    DISCOUNT: (period: number, rate: number) => `${period}간 ${rate}% 할인받기`,
+  },
+
+  ADMIN: {
+    SAVE: "저장하기",
+    UPDATE: "수정하기",
+  },
+};
