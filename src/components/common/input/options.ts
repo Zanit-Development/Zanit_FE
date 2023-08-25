235eff3e4f235c9855d5bb2a11cccc373a6f5f30
ungHeung <t01047377477@gmail.com>
2023-08-25 15:51:03 +0900

    feat : 인풋 옵션 추가

---
+++ src/components/common/input/options.ts
@@ -0,0 +1,10 @@
+/**
+ * 인풋 인터페이스, 타입 옵션
+ */
+
+import { INPUT_SIZE_VARIANTS, INPUT_TYPE_VARIANTS } from "../../../libs/interface/typeCommon";
+
+export interface StyledInputOption {
+  typevariants: INPUT_TYPE_VARIANTS;
+  sizevariants: INPUT_SIZE_VARIANTS;
+}
