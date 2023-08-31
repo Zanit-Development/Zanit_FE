cffa8563daeb195a765fc34bcf70de9a1486dc5d
ungHeung <t01047377477@gmail.com>
2023-08-31 17:26:37 +0900

    #77 feat : 셀렉트 기능, 옵션 추가

---
+++ src/components/common/select/selectOptions.ts
@@ -0,0 +1,6 @@
+import { SELECT_SIZE_VARIANTS, SELECT_TYPE_VARIANTS } from "../../../libs/interface/typeCommon";
+
+export interface StyledSelectOption {
+  typevariants: SELECT_TYPE_VARIANTS;
+  sizevarignts: SELECT_SIZE_VARIANTS;
+}
