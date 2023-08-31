45624b016f1fac8b2321dbf4033f34364a48ceaf
jypark38 <jury124@naver.com>
2023-09-01 00:27:49 +0900

    (#62) chore : 팝업 관련 타입

---
+++ src/libs/interface/interfaceUseCoupon.ts
@@ -0,0 +1,9 @@
+export interface PopupProps {
+  barimg: string;
+  barname: string;
+  barlocation: string;
+  cocktailname: string;
+  cocktailprice: number;
+  covercharge?: number;
+  onClose: () => void;
+}
