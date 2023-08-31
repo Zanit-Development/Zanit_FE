a5c69c7c1bb06ae2608ae8c141b3f6cde3ca3b8a
jypark38 <jury124@naver.com>
2023-09-01 00:27:16 +0900

    (#62) feat : 팝업 띄우기 버튼

---
+++ src/components/useCoupon/ShowPopupButton.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useState } from "react";
+
+import Button from "../common/button/Button";
+import { ButtonProps } from "../../libs/interface/interfaceCommon";
+import { Popup } from "./UseCouponPopup";
+
+import bar1 from "../../assets/sample-img/bar1.png";
+import { PopupProps } from "../../libs/interface/interfaceUseCoupon";
+
+const ShowPopupButton = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  // 닫기 esc 필요?
+  const onClose = useCallback(() => {
+    setShowPopup(false);
+  }, []);
+
+  const buttonProps: ButtonProps = {
+    typevariants: "fill",
+    sizevariants: "small",
+    value: "ZAN 쿠폰 사용하기",
+    disabled: false,
+    onClick: () => {
+      // 누르면 popup 마운트
+      setShowPopup(true);
+    },
+  };
+
+  const PopupOption: PopupProps = {
+    barimg: bar1,
+    barname: "바 티센트",
+    barlocation: "서울 강남구 선릉로162길 16 엘리자벳빌딩 1층",
+    cocktailname: "깔루아 밀크",
+    cocktailprice: 28000,
+    // covercharge: 10000,
+    onClose: onClose,
+  };
+
+  return (
+    <>
+      <Button {...buttonProps} />
+      {showPopup && <Popup {...PopupOption} />}
+    </>
+  );
+};
+
+export default ShowPopupButton;
