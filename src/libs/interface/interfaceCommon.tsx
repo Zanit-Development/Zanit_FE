0b0f6d77ba60bd1e0720a42a6f19381d08934c90
ungHeung <t01047377477@gmail.com>
2023-08-31 17:23:37 +0900

    fix : 인풋 타입 직관적으로 변경

--- src/libs/interface/interfaceCommon.tsx
+++ src/libs/interface/interfaceCommon.tsx
@@ -2,18 +2,8 @@
  * 인터페이스
  */
 
-import {
-  BUTTON_TYPE_VARIANTS,
-  BUTTON_SIZE_VARIANTS,
-  INPUT_TYPE_VARIANTS,
-  INPUT_SIZE_VARIANTS,
-  TAG_TYPE_VARIANTS,
-  ITEM_TYPE_VARIANTS,
-  MODAL_TYPE_VARIANTS,
-  SELECT_TYPE_VARIANTS,
-  SELECT_SIZE_VARIANTS,
-} from "./typeCommon";
-import { BUTTON_EVENT, INPUT_EVENT, MOUSE_EVENT } from "./typeEvent";
+import { BUTTON_TYPE_VARIANTS, BUTTON_SIZE_VARIANTS, INPUT_TYPE_VARIANTS, INPUT_SIZE_VARIANTS, TAG_TYPE_VARIANTS, ITEM_TYPE_VARIANTS, SELECT_TYPE_VARIANTS, SELECT_SIZE_VARIANTS } from "./typeCommon";
+import { BUTTON_EVENT, INPUT_EVENT, MOUSE_EVENT, SELECT_EVENT } from "./typeEvent";
 
 export interface AxiosOptions {
   timeout?: number;
@@ -70,6 +60,6 @@ export interface SelectProps {
   sizevariants: SELECT_SIZE_VARIANTS;
   id?: string;
   options: string[];
-  value: string[];
-  onChange: Function;
+  values: string[];
+  onChange: (e: SELECT_EVENT) => void;
 }
--- src/libs/interface/typeCommon.tsx
+++ src/libs/interface/typeCommon.tsx
@@ -5,7 +5,7 @@
 export type BUTTON_TYPE_VARIANTS = "fill" | "stroke";
 export type BUTTON_SIZE_VARIANTS = "large" | "small";
 
-export type INPUT_TYPE_VARIANTS = "primary" | "secondary";
+export type INPUT_TYPE_VARIANTS = "basic" | "search";
 export type INPUT_SIZE_VARIANTS = "large" | "medium" | "small";
 
 export type TAG_TYPE_VARIANTS = "primary" | "secondary" | "tertiary";
