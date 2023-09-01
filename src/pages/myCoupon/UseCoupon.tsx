2f7ce73d58c27566323d84d06998950514ea0b6a
jypark38 <jury124@naver.com>
2023-09-01 23:13:42 +0900

    (#62) add : 쿠폰페이지 기능 추가

--- src/pages/myCoupon/UseCoupon.tsx
+++ src/pages/myCoupon/UseCoupon.tsx
@@ -1,65 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { styled } from "styled-components";
 import Layout from "../../layouts/Layout";
 import ShowPopupButton from "../../components/useCoupon/ShowPopupButton";
 
-import ArrowIcon from "../../assets/icon/icon_arrow_down.svg";
-import BarSelectBox from "./BarSelectBox";
-import SelectBox from "./SelectBox";
+import SelectBox from "../../components/common/select-box/SelectBox";
 
 const UseCoupon = () => {
   const barList = ["bar1", "bar2", "bar3", "bar4", "bar5", "bar6", "bar7", "bar8"];
   const [selectedBar, setSelectedBar] = useState("");
-  const CocktailList = ["cock1", "cock2", "cock3"];
+  const CocktailList: { [prop: string]: string[] } = useMemo(
+    () => ({
+      bar1: ["cock1", "cock2", "cock3", "cock4", "cock5"],
+      bar2: ["1", "2", "3", "4", "5"],
+    }),
+    []
+  );
   const [selectedCocktail, setSelectedCocktail] = useState("");
+
+  useEffect(() => {
+    setSelectedCocktail("");
+  }, [selectedBar]);
+
+  const BarOptions = {
+    bgcolor: "#F4F4F4",
+    selected: selectedBar,
+    setSelected: setSelectedBar,
+    data: barList,
+    placeholder: "바 이름을 검색해 보세요",
+    nulltext: "바 선택하기",
+  };
+
+  const CocktailOptions = {
+    bgcolor: "#F4F4F4",
+    selected: selectedCocktail,
+    setSelected: setSelectedCocktail,
+    data: CocktailList[selectedBar] || [],
+    placeholder: "칵테일을 선택해 주세요",
+    nulltext: "칵테일 선택하기",
+  };
+
   return (
     <Layout>
       <MainContainer>
         <h2>쿠폰 사용하기</h2>
         <h3>어떤 바를 방문하셨나요?</h3>
-        <BarSelectBox selected={selectedBar} setSelected={setSelectedBar} data={barList} />
+        <SelectBox {...BarOptions} />
         <h3>어떤 칵테일을 마셨나요?</h3>
-        <SelectBox selected={selectedCocktail} setSelected={setSelectedCocktail} data={!selectedBar ? [] : CocktailList} />
-
-        {/* 
-        <form action="">
-          <select name="" id="bars" onChange={(e) => setSelectedBar(e.target.value)}>
-            <option value="" disabled selected hidden>
-              바 이름을 검색해보세요
-            </option>
-            <option disabled>바 선택하기</option>
-            {barList.map((item) => (
-              <option key={item} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-          <select name="" id="cocktails" onChange={(e) => setSelectedCocktail(e.target.value)}>
-            <option value="" disabled selected hidden>
-              칵테일을 선택해 주세요
-            </option>
-            {selectedBar ? (
-              <>
-                <option value="" disabled>
-                  바 선택하기
-                </option>
-                {CocktailList.map((item) => {
-                  return <option value={item}>{item}</option>;
-                })}
-              </>
-            ) : (
-              <option disabled>방문하신 Bar를 선택해주세요</option>
-            )}
-          </select>
-        </form> */}
-        <ShowPopupButton />
+        <SelectBox {...CocktailOptions} />
+        <ShowPopupButton bar={selectedBar} cock={selectedCocktail} />
       </MainContainer>
     </Layout>
   );
 };
 
 const MainContainer = styled.div`
-  margin: 30px 20px 239px;
+  margin: 30px 20px 35px;
 
   h2 {
     font-family: var(--font--Medium);
@@ -76,7 +71,7 @@ const MainContainer = styled.div`
   }
 
   button {
-    margin-top: calc(59px);
+    margin-top: calc(25px + 202px);
   }
 `;
 
