48fcb350232ed78005bb4f5ff035b37768533f3a
ungHeung <t01047377477@gmail.com>
2023-09-05 08:12:48 +0900

    fix : 충돌 수정

--- src/pages/admin/bar/BarManagement.tsx
+++ src/pages/admin/bar/BarManagement.tsx
@@ -1,14 +1,37 @@
 import { ManageInfo } from "./ManageInfo";
 import AdminLayout from "../../../layouts/adminLayout/AdminLayout";
+import { styled } from "styled-components";
 
 const BarManagement = () => {
   return (
     <AdminLayout>
-      <section>
+      <StyledMenu>
         <ManageInfo />
-      </section>
+      </StyledMenu>
     </AdminLayout>
   );
 };
+
+const StyledMenu = styled.menu`
+  width: calc(100% - 40px);
+  margin-bottom: 20px;
+  padding: 0 20px;
+
+  & > input {
+    display: none;
+  }
+
+  & > label {
+    margin-right: 20px;
+    font-family: var(--font--Bold);
+    color: var(--gray200-color);
+    cursor: pointer;
+  }
+
+  & > input:checked + label {
+    border-bottom: 2px solid var(--main-color);
+    color: black;
+  }
+`;
 
 export default BarManagement;
