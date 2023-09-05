109f2157978aea94a4d30df4c9efc68de497318a
ungHeung <t01047377477@gmail.com>
2023-09-06 01:16:47 +0900

    feat : 칵테일 아이템 컴포넌트 추가

---
+++ src/pages/admin/bar/CocktailItem.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { styled } from "styled-components";
+import Cocktail from "../../../components/bardetail/Cocktail";
+import updateCocktailImg from "../../../assets/icon/icon_update_cocktail_button.svg";
+import closeButton from "../../../assets/icon/icon_close.svg";
+
+export const CocktailItem = ({ ...props }) => {
+  return (
+    <li>
+      <button>
+        <img src={closeButton} alt="" />
+      </button>
+      <button>
+        <img src={updateCocktailImg} alt="" />
+      </button>
+      <Cocktail info={{ img: "string", title: "string", level: "string", description: "string" }} idx={0} />
+      <StyledCheckbox id="sam1" type="checkbox" />
+      <label htmlFor="sam1"></label>
+    </li>
+  );
+};
+
+const StyledCheckbox = styled.input`
+  & + label {
+    position: relative;
+    display: block;
+    width: 24px;
+    height: 13px;
+    float: right;
+    background-color: var(--gray300-color);
+    border-radius: 7px;
+    cursor: pointer;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: -1.5px;
+      width: 16px;
+      height: 16px;
+      border-radius: 8px;
+      box-shadow: 0 3px 5px rgba(0, 0, 0, 0.5);
+      transition: 0.3s;
+    }
+  }
+
+  &:checked + label {
+    background-color: rgba(241, 76, 41, 0.5);
+  }
+
+  &:checked + label::before {
+    left: 16px;
+    background-color: var(--main-color);
+  }
+
+  &:not(:checked) + label::before {
+    left: -6.5px;
+    background-color: var(--gray100-color);
+  }
+`;
