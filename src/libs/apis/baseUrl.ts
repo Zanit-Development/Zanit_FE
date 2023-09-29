314172043d35d502c8f2527d1bf09ada90bbe80b
jypark38 <jury124@naver.com>
2023-09-29 23:17:27 +0900

    chore : baseURL 변경

--- src/libs/apis/baseUrl.ts
+++ src/libs/apis/baseUrl.ts
@@ -2,4 +2,4 @@
  * 기본 URL
  */
 
-export const BASE_URL = "http://ec2-13-124-124-221.ap-northeast-2.compute.amazonaws.com:8080/";
+export const BASE_URL = "http://zanit.eastus.cloudapp.azure.com:8080/";
