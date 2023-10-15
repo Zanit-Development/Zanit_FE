b50b6b516400caed68fa45fb271821123f896aba
jypark38 <jury124@naver.com>
2023-10-15 17:55:33 +0900

    remove : setScreenSize in Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -9,13 +9,6 @@ type WrapperProps = {
 };
 
 const Layout = ({ children }: WrapperProps) => {
-  function setScreenSize() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
-
-  setScreenSize();
-
   return (
     <LayoutWrap>
       <FixHeader>
