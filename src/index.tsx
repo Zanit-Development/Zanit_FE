051ddcf92016a6a058809e9f48b98e07dc86dde7
jypark38 <jury124@naver.com>
2024-01-01 18:13:21 +0900

    strictmode 주석

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RecoilRoot>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  </RecoilRoot>
+  // </React.StrictMode>
 );
